61a0c4361b4d46c5b1a01d7641157ef24cfc1f1b
ida16 <34029551+ida16@users.noreply.github.com>
2018-10-04 12:44:59 +0000

    Returns number when one number is in string

--- src/calculator.js
+++ src/calculator.js
@@ -1,6 +1,8 @@
 function add (number){
     if(number == "")
-    return 0;
+        return 0;
+
+    return parseInt(number);
 }
 
 module.exports = add;
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,5 +1,9 @@
-const add = require("./src/calculator");
+const add = require("./calculator");
 
 it("should return zero on an empty string", () => {
-    expected(add("")).toBe(0);
-})
+    expect(add("")).toBe(0);
+});
+
+it("should return number when only one number is in the string", () => {
+    expect(add("3")).toBe(3);
+});
