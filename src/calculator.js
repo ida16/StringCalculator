2cf94e3257fe665204cc59fd3ed6a1f05d7a1b1f
ida16 <34029551+ida16@users.noreply.github.com>
2018-10-10 13:27:23 +0000

    Handles lines between numbers

--- src/calculator.js
+++ src/calculator.js
@@ -1,27 +1,30 @@
 function add (numbers){
+
+    var numbers = numbers.replace(/\n|;|\/|\\|[.*]|[%]|[$]/g,",");
+
     if(numbers == "")
         return 0;
 
-    if(numbers.includes(","))
-    {
+    if(numbers != '') {
         var numberArray = numbers.split(",");
-        return sum(numberArray);
-    }
-    else if(numbers.includes(",|n"))
-    {
-        var returnValue = numbers.split(",|n");
-        return newLine(returnValue);
-    }
+        return sum(numberArray, ',');
+        }
+        
     else
         return parseInt(numbers);
+    
 }
 
 function sum(numberArray){
     var total = 0;
+    var negativeArr = [];
+
     for(var i=0; i < numberArray.length; i++) {
         total += parseInt(numberArray[i]);
         }
+
     return total;
 }
+
 
 module.exports = add;
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -15,3 +15,7 @@ it("should return sum of two given numbers", () => {
 it("should handle an unknown number of arguments", () => {
     expect(add("1,2,3,4,5,6,7,8,9,10")).toBe(55);
 })
+
+it("should handle new lines between numbers", () => {
+    expect(add("1\n2,3")).toBe(6);
+})
